612ad7f27a8ae1cb667132f3c175d15fd5447696
Arthur Noguera <dev@nogadev.ch>
2024-11-12 15:29:45 +0100

    test

--- app/Livewire/Components/Footer.php
+++ app/Livewire/Components/Footer.php
@@ -6,7 +6,7 @@ use Livewire\Component;
 
 class Footer extends Component
 {
-    public function render()
+    public function render(): \Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\View\View
     {
         return view('livewire.components.footer');
     }
