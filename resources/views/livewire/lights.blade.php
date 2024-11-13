2ec2eebc29a3cc2e8c3f1ccfcf78d76b1a8b43a0
Arthur Noguera <dev@nogadev.ch>
2024-11-13 16:58:40 +0100

    change view

--- resources/views/livewire/lights.blade.php
+++ resources/views/livewire/lights.blade.php
@@ -2,7 +2,7 @@
 
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-4">
         @foreach($lights as $light)
-            <livewire:components.card
+            <livewire:Components.card
                 :material="$light"
                 :cat="$cat"
             />
--- resources/views/livewire/technique.blade.php
+++ resources/views/livewire/technique.blade.php
@@ -2,7 +2,7 @@
 
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-4">
         @foreach($techs as $tech)
-            <livewire:components.card
+            <livewire:Components.card
 
                 :material="$tech"
                 :cat="$cat"
