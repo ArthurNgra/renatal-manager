cef7d519182c0ba4d654c2f53efda63806683666
Arthur Noguera <dev@nogadev.ch>
2024-11-17 12:05:10 +0100

    test contact

--- app/Livewire/Pages/Contact.php
+++ app/Livewire/Pages/Contact.php
@@ -36,32 +36,22 @@ class Contact extends Component
 
         $validated = $this->validate();
 
-        try {
-            // Logique d'envoi de mail
-            Mail::send('emails.contact', [
-                'name' => $this->name,
-                'email' => $this->email,
-                'messageContent' => $this->message,
-            ], function ($mail) {
-                $mail->to($this->email)
-                    ->subject('Demande de contact')
-                    ->from(config('mail.mailers.smtp.username'), config('mail.from'));
-            });
+        // Logique d'envoi de mail
+        Mail::send('emails.contact', [
+            'name' => $this->name,
+            'email' => $this->email,
+            'messageContent' => $this->message,
+        ], function ($mail) {
+            $mail->to($this->email)
+                ->subject('Demande de contact')
+                ->from(config('mail.mailers.smtp.username'), config('mail.from'));
+        });
 
-            (new ContactService())->create($validated);
-            session()->flash('success', 'Votre message a bien été transmis');
-            $this->reset();
-        } catch (\Symfony\Component\Mailer\Exception\TransportExceptionInterface $e) {
-            if (str_contains($e->getMessage(), '554')) {
-                session()->flash('error', 'Impossible d\'envoyer le message. Veuillez vérifier votre adresse email ou réessayer plus tard.');
-            } else {
-                session()->flash('error', 'Une erreur est survenue lors de l\'envoi du message. Veuillez réessayer plus tard.');
-            }
-        } catch (\Exception $e) {
-            session()->flash('error', 'Une erreur inattendue est survenue. Veuillez réessayer plus tard.');
-        }
+
+        (new ContactService())->create($validated);
+        session()->flash('success', 'Votre message a bien été transmis');
+        $this->reset();
     }
-
 
     public function render()
     {
